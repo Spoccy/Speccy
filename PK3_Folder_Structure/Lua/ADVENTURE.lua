6e745f9602ed56fbb72b4f9236a935d7a5a0ee9a
Speccy <106514463+Spoccy@users.noreply.github.com>
2022-05-31 16:09:36 -0500

    Update ADVENTURE.lua

--- PK3_Folder_Structure/Lua/ADVENTURE.lua
+++ PK3_Folder_Structure/Lua/ADVENTURE.lua
@@ -13,29 +13,30 @@ rawset(_G, "levelSpeccyUp", function(p)
 	if p.mo and p.mo.valid then
 		if p.mo.skin == "speccy" then
 			p.speccy.level = $ + 1
-			p.speccy.exp = 0
+			p.speccy.exp = $ - p.speccy.expreq
 			p.speccy.expreq = newExpRequirement(p)
-			p.speccy.maxhp
+			p.speccy.maxhp = $ + 15 * (p.speccy.maxhp % (p.speccy.level+1))
 		else
 			error("cannot level up if you're not Speccy!", 2)
 		end
 	else
 		error("cannot level up if you don't exist!", 2)
 	end
-end
+end)
 
 -- Boss Table, supports custom bosses by adding them after Speccy... maybe you can add them before, I don't know, maybe by rawsetting it yourself.
+-- This is carried over from before the re-script.
 
 -- Layout: [MT_MOBJNAME] = {"Boss Name", Damage, EXP reward}
 
 rawset(_G, "adventureBossTable", {
-	[MT_EGGMOBILE] = {"Egg Zapper", 10, 25},
-	[MT_EGGMOBILE2] = {"Egg Slimer", 10, 45},
-	[MT_EGGMOBILE3] = {"Sea Egg", 15, 70},
-	[MT_EGGMOBILE4] = {"Egg Colosseum", 15, 95},
-	[MT_FANG] = {"Fang the Sniper", 25, 135},
-	[MT_METALSONIC_BATTLE] = {"Metal Sonic", 35, 165},
-	[MT_CYBRAKDEMON] = {"Black Eggman", 50, 200},
+	[MT_EGGMOBILE] = {"Egg Zapper", 25, 75},
+	[MT_EGGMOBILE2] = {"Egg Slimer", 35, 150},
+	[MT_EGGMOBILE3] = {"Sea Egg", 45, 175},
+	[MT_EGGMOBILE4] = {"Egg Colosseum", 50, 250},
+	[MT_FANG] = {"Fang the Sniper", 35, 500},
+	[MT_METALSONIC_BATTLE] = {"Metal Sonic", 50, 750},
+	[MT_CYBRAKDEMON] = {"Black Eggman", 100, 1000},
 	[MT_CYBRAKDEMON_ELECTRIC_BARRIER] = {"Black Eggman's Barrier", 25, 0},
 })
 
@@ -46,7 +47,7 @@ rawset(_G, "resetSpeccyVars", function(p)
 			p.speccy.maxhp = 100
 			p.speccy.hp = p.speccy.maxhp
 			p.speccy.level = 1
-			p.speccy.xp = 0
+			p.speccy.exp = 0
 			p.speccy.expreq = newExpRequirement(p)
 		else
 			error("cannot run resetSpeccyVars if you're not Speccy!", 2)
@@ -62,7 +63,64 @@ addHook("PlayerThink", function(p)
 			if p.speccy == nil then
 				resetSpeccyVars(p)
 			else
-				print(p.speccy.expreq)
+			
+				if p.speccy.exp >= p.speccy.expreq then
+					levelSpeccyUp(p)
+				end
+			
+			end
+		end
+	end
+end)
+
+-- Damage Handler
+
+addHook("ShouldDamage", function(pmo, inf, mo, dmg, dmgtype)
+	if pmo and pmo.valid then
+		if pmo.skin == "speccy" then
+			if pmo.player then
+				local p = pmo.player
+				if inf and inf.valid then
+					if src and src.valid then
+						if inf.type == src.type then
+							if (src.flags & MF_BOSS) then
+								if adventureBossTable[mo.type] ~= nil then
+									p.speccy.hp = $ - adventureBossTable[mo.type][2]
+								else
+									p.speccy.hp = $ - 25
+								end
+								
+								P_DoPlayerPain(p, mo, inf)
+								p.powers[pw_flashing] = 3*TICRATE
+							elseif (src.flags & MF_ENEMY) then
+								p.speccy.hp = $ - P_RandomRange(5,10)
+							end
+						else
+							p.speccy.hp = $ - P_RandomRange(10,15)
+						end
+					else
+						p.speccy.hp = $ - P_RandomRange(10,15)
+					end
+				end
+				return false
+			end
+		end
+	end
+end)
+
+-- EXP Handler
+
+addHook("MobjDeath", function(mo, inf, pmo)
+	if pmo and pmo.valid then
+		if pmo.player and pmo.player.speccy then
+			if (mo.flags & MF_BOSS) then
+				if adventureBossTable[mo.type] ~= nil then
+					pmo.player.speccy.exp = $ + adventureBossTable[mo.type][3]
+				else
+					pmo.player.speccy.exp = $ + 25
+				end
+			elseif (mo.flags & MF_ENEMY) then
+				pmo.player.speccy.exp = $ + P_RandomRange(2,5)
 			end
 		end
 	end
@@ -76,3 +134,44 @@ addHook("MobjMoveCollide", function(mo, pmo)
         mo.flags2 = $|MF2_CLASSICPUSH
     end
 end, MT_METALSONIC_BATTLE)
+
+-- Hud stuff at the very bottom.
+-- Seeing as people are going to be reading this maybe, I'll post on how to make a HUD.
+
+-- In order to make a HUD, start with checking for the below:
+
+if not speccyCustomHud then
+  rawset(_G, "speccyCustomHud", {})
+end
+
+-- It makes sure that Speccy exists, and that her customHuds table exists.
+
+-- Part 1: The Actual HUD.
+
+-- Starting with the actual HUD here.
+-- You'll be wanting to read a lot of Speccy's stats here so I made a list for you!
+
+/*
+
+    p.speccy	- This is your main array, always check for this after making sure the player exists.
+	p.speccy.hp - Your health.
+	p.speccy.maxhp - Your maximum health.
+	p.speccy.level - Your level.
+	p.speccy.exp   - Your current EXP count. 
+	p.speccy.expreq   - EXP needed to reach next level. p.exqreq - p.exp to get EXP left.
+
+*/
+
+-- ZELDA II-esque HUD
+
+speccyCustomHud[#speccyCustomHud+1] = function(v, p)
+
+	-- Black Bar for that nice feel.
+	
+	v.drawFill(0, 0, 320, 24, 31)
+
+	-- HP BAR, on the left because Speccy has no Magic.
+
+end
+
+hud.add(speccyCustomHud[1], "game")
